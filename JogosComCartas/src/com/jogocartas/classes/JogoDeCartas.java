2174d4c5b48fc4d54f5d521b3f8b4b2590692bab
Altamir Benkenstein <altamir.benkenstein@outlook.com>
2013-10-19 17:05:52 -0300

    Criado classes Cartas, Baralho, ENUN.

---
+++ JogosComCartas/src/com/jogocartas/classes/JogoDeCartas.java
@@ -0,0 +1,13 @@
+package com.jogocartas.classes;
+
+public interface JogoDeCartas {
+	
+	public void ordenar();
+	
+	public void embaralhar();
+	
+	public Carta darCartas();
+	
+	public String listarBaralho();
+
+}
